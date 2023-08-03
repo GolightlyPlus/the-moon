2b793ba56bfb52d57970f6c22c150bfc8565b759
magician11 <support@andrewgolightly.com>
2023-08-03 21:36:18 +0100

    completed it

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Moon
+
+## Credits
+
+Background photo: https://unsplash.com/photos/rCbdp8VCYhQ
+Icon: https://www.svgrepo.com/svg/484307/moon
+API:
---
+++ src/App.js
@@ -0,0 +1,66 @@
+import '@fontsource/inter';
+import {
+  CssBaseline,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  LinearProgress
+} from '@mui/joy';
+import { CssVarsProvider } from '@mui/joy/styles';
+
+import background from './images/space.jpeg';
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  const [moonData, setMoonData] = useState();
+
+  useEffect(() => {
+    const fetchMoonPhase = async () => {
+      const res = await fetch(
+        `https://api.farmsense.net/v1/moonphases/?d=${Math.floor(
+          Date.now() / 1000
+        )}`
+      );
+      const data = await res.json();
+      setMoonData(data[0]);
+    };
+
+    fetchMoonPhase();
+  }, []);
+
+  if (!moonData) {
+    return <LinearProgress />;
+  }
+
+  return (
+    <CssVarsProvider defaultMode="system">
+      <CssBaseline />
+      <Grid
+        container
+        sx={{
+          backgroundImage: `url(${background})`,
+          height: '100vh',
+          backgroundSize: 'cover'
+        }}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid>
+          <Card color="primary" variant="soft">
+            <CardContent>
+              <Typography level="h3">
+                Moon phase: {moonData.Illumination * 100}%
+              </Typography>
+              <Typography level="body-lg" textAlign={'center'}>
+                {moonData.Phase}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </CssVarsProvider>
+  );
+};
+
+export default App;
